8ab20d8c6af72cb52556568fe987372c5964dff3
Lucas Cavalli <108158600+zPolda@users.noreply.github.com>
2023-06-03 22:24:27 -0300

    add api full project

---
+++ src/main/java/med/vol/api/controller/MedicoController.java
@@ -0,0 +1,25 @@
+package med.vol.api.controller;
+
+import med.vol.api.endereco.Endereco;
+import med.vol.api.medico.DadosCadastroMedico;
+import med.vol.api.medico.Medico;
+import med.vol.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+    @Autowired
+    private MedicoRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody DadosCadastroMedico dados){
+        repository.save(new Medico(dados));
+    }
+}
